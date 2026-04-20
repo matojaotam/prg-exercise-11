7710cc1aec67859c765cc0b5c30b22686cf76668
matojaotam <lalokonosec1@gmail.com>
2026-04-20 12:29:52 +0200

    nestabilni algoritmus

---
+++ sorting.py
@@ -0,0 +1,21 @@
+# bubble_sort(), selection_sort() a random_numbers()
+
+import random
+
+def random_numbers(count, low=0, high=100):
+    return [random.randint(low, high) for _ in range(count)]
+
+values = random_numbers(10)  # 10 čísel v rozsahu 0–100
+print(values)  # např. [42, 7, 91, 15, 63, 8, 57, 73, 2, 100]
+
+# small = random_numbers(5, low=0, high=20)  # 5 čísel v rozsahu 0–20
+# print(small)
+for i in range(len(values)):
+   min_num = i
+
+   for j in range(i + 1, len(values)):
+       if values[j] < values[min_num]:
+           min_num = j
+   values[i], values[min_num] = values[min_num], values[i]
+
+print(values)
